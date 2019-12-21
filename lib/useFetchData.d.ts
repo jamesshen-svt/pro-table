8ce77792027a61efc3e8c3b31012fe93550357b4
James Shen <james.shen@sageviewtech.com>
2019-12-21 19:53:31 +1100

    fix: add lib

---
+++ lib/useFetchData.d.ts
@@ -0,0 +1,31 @@
+export interface RequestData<T> {
+    data: T[];
+    success?: boolean;
+    total?: number;
+}
+export interface UseFetchDataAction<T extends RequestData<any>> {
+    dataSource: T['data'] | T;
+    loading: boolean;
+    hasMore: boolean;
+    current: number;
+    pageSize: number;
+    total: number;
+    fetch: () => Promise<void>;
+    reload: () => Promise<void>;
+    fetchMore: () => void;
+    fullScreen?: () => void;
+    resetPageIndex: () => void;
+    setCurrent: (current: number) => void;
+    setPageSize: (pageSize: number) => void;
+    restColumnsConfig?: () => void;
+}
+declare const useFetchData: <T extends RequestData<any>, U = {}>(getData: (params: {
+    pageSize: number;
+    current: number;
+}) => Promise<T>, defaultData?: Partial<T["data"]> | undefined, options?: {
+    defaultCurrent?: number | undefined;
+    defaultPageSize?: number | undefined;
+    effects?: any[] | undefined;
+    onLoad?: ((dataSource: T["data"]) => void) | undefined;
+} | undefined) => UseFetchDataAction<T>;
+export default useFetchData;
